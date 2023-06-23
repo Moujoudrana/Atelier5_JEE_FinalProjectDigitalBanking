1d34891fbcd396e7241a0da5d04e26a97f285d72
Moujoudrana <MOUJOUD_RANA@emsi-edu.ma>
2023-06-24 00:32:12 +0100

    first commit

---
+++ Backend/src/main/java/com/example/atelier4/repositories/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.atelier4.repositories;
+
+import com.example.atelier4.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    List<Customer> findByNameContains(String keyword);
+}
